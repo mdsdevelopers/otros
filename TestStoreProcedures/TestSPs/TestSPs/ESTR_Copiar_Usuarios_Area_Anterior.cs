06d81f95f09011ecf97d32c4b31faa1b709c8fdd
mtejeira <matiassaenztejeira@gmail.com>
2014-10-09 20:21:13 -0300

    agregado de clases.

---
+++ TestStoreProcedures/TestSPs/TestSPs/ESTR_Copiar_Usuarios_Area_Anterior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace TestStoredProcedures
+{
+    public class ESTR_Copiar_Usuarios_Area_Anterior:StoredProcedure
+    {
+
+        public ESTR_Copiar_Usuarios_Area_Anterior(string nombre, SqlConnection conexion_del_sp, LoggerEjecucionSps logger_sp)
+            : base(nombre, conexion_del_sp, logger_sp)
+        {
+
+        }
+
+        protected override bool CompletarParametroEspecifico(SqlParameter parametro)
+        {
+            var result = base.CompletarParametroEspecifico(parametro);
+            switch (parametro.ParameterName)
+            {
+                case "@IdAreaAnterior":
+                    parametro.Value = 1234;
+                    return true;
+                case "@IdAreaNueva":
+                    parametro.Value = 4321;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
