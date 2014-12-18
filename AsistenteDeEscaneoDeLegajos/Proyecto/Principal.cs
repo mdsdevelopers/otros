a06b29a25e92b575491a98369b19204fdfc9a8da
unknown <clurgo@NDJ-1208.sds_domain_1.local>
2014-12-17 21:46:37 -0300

    BuscadorConcursar

--- AsistenteDeEscaneoDeLegajos/Proyecto/Principal.cs
+++ AsistenteDeEscaneoDeLegajos/Proyecto/Principal.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using General.Modi;
 using General.Repositorios;
@@ -16,7 +10,7 @@ namespace AsistenteDeEscaneoDeLegajos
         private ConexionBDSQL conexion_db;
         private FileSystem file_system;
         private ServicioDeCompresionYPersistenciaDeImagenes servicio_de_subida;
-
+        private string path = @"\\ZEUS\RRHHyORG\Organización\DigitalizacionLegajos";
         public Principal()
         {
             InitializeComponent();
@@ -24,16 +18,26 @@ namespace AsistenteDeEscaneoDeLegajos
 
         private void Principal_Load(object sender, EventArgs e)
         {
-            this.conexion_db = new ConexionBDSQL("Data Source=SQLTRETA;Initial Catalog=DB_RRHH;Integrated Security=True");
-            this.file_system = new FileSystem();
-            this.servicio_de_subida = new ServicioDeCompresionYPersistenciaDeImagenes(conexion_db, 
-                file_system, 
-                "\\\\ZEUS\\RRHHyORG\\Organización\\DigitalizacionLegajos");
+            this.textBox1.Text = this.path;
         }
 
         private void btnSubirImagenes_Click(object sender, EventArgs e)
         {
+
+            this.conexion_db = new ConexionBDSQL("Data Source=" + this.comboBox1.Text + ";Initial Catalog=DB_RRHH;Integrated Security=True");
+            this.file_system = new FileSystem();
+            this.servicio_de_subida = new ServicioDeCompresionYPersistenciaDeImagenes(conexion_db,
+                file_system,
+                path);
             servicio_de_subida.AgregarImagenesSinAsignarDeUnLegajoALaBase(Int32.Parse(this.txtIdInterna.Text));
+            MessageBox.Show("Listo");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog1.ShowDialog();
+            this.path = this.folderBrowserDialog1.SelectedPath;
+            this.textBox1.Text = this.path;
         }
     }
 }
