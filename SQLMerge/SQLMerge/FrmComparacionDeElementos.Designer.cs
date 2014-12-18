bffb2795e24cd26aee17733cc8deecb31171e4a9
aazaa <arielzambrano@gmail.com>
2014-12-17 22:28:55 -0300

    agregado el resaltado de las lìneas diferentes en la pantalla de comparaciòn de textos

--- SQLMerge/SQLMerge/FrmComparacionDeElementos.Designer.cs
+++ SQLMerge/SQLMerge/FrmComparacionDeElementos.Designer.cs
@@ -29,38 +29,13 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmComparacionDeElementos));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lbl_longitud_elemento_1 = new System.Windows.Forms.Label();
             this.lbl_longitud_elemento_2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(12, 32);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(474, 463);
-            this.textBox1.TabIndex = 0;
-           
-            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.White;
-            this.textBox2.Location = new System.Drawing.Point(495, 32);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(474, 463);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
             // 
             // label6
             // 
@@ -102,17 +77,33 @@
             this.lbl_longitud_elemento_2.TabIndex = 126;
             this.lbl_longitud_elemento_2.Text = "0";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(474, 463);
+            this.textBox1.TabIndex = 127;
+            this.textBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(495, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(474, 463);
+            this.textBox2.TabIndex = 128;
+            this.textBox2.Text = "";
+            // 
             // FrmComparacionDeElementos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 537);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lbl_longitud_elemento_2);
             this.Controls.Add(this.lbl_longitud_elemento_1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -120,17 +111,16 @@
             this.Text = "Comparación de elementos";
             this.Load += new System.EventHandler(this.FrmComparacionDeElementos_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbl_longitud_elemento_1;
         private System.Windows.Forms.Label lbl_longitud_elemento_2;
+        private System.Windows.Forms.RichTextBox textBox1;
+        private System.Windows.Forms.RichTextBox textBox2;
     }
 }
--- SQLMerge/SQLMerge/FrmComparacionDeElementos.cs
+++ SQLMerge/SQLMerge/FrmComparacionDeElementos.cs
@@ -7,13 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using DiffMatchPatch;
 
 namespace SQLMerge
 {
     public partial class FrmComparacionDeElementos : Form
     {
         
-        
+        public struct Chunk
+        {
+            public int startpos;
+            public int length;
+            public Color BackColor;
+        }
+
+        diff_match_patch DIFF = new diff_match_patch();
+        // these are the diffs
+        List<Diff> diffs;
+
+        // chunks for formatting the two RTBs:
+        List<Chunk> chunklist1;
+        List<Chunk> chunklist2;
+
+        // two color lists:
+        Color[] colors1 = new Color[3] { Color.LightGreen, Color.LightSalmon, Color.White, };
+        Color[] colors2 = new Color[3] { Color.LightSalmon, Color.LightGreen, Color.White };
+
+
+
         public void MostrarElementos(string elemento1, string elemento2)
         {
             textBox1.ShortcutsEnabled = true;
@@ -21,9 +42,91 @@ namespace SQLMerge
             textBox1.Text = elemento1;
             lbl_longitud_elemento_1.Text = elemento1.Length.ToString();  
             textBox2.Text = elemento2;
-            lbl_longitud_elemento_2.Text = elemento2.Length.ToString();  
-        
+            lbl_longitud_elemento_2.Text = elemento2.Length.ToString();
+
+            //foreach (var linea_texto1 in textBox1.Lines)
+            //{
+
+            //    foreach (var linea_texto2 in textBox2.Lines)
+            //    {
+
+
+            //        if (linea_texto1 != linea_texto2)
+            //        {
+
+            //            int blockStart = 0;
+            //            int blockLength =linea_texto1.Length;
+                      
+            //            textBox1.SelectionStart = blockStart;
+            //            textBox1.SelectionLength = blockLength;
+            //            textBox1.SelectionBackColor = Color.Yellow;
+            //        }
+                    
+            //    }
+                
+            //}
+
+            
+            //int blockStart = 1; //arbitrary numbers to test
+            //int blockLength = 15;
+            //textBox1.SelectionStart = blockStart;
+            //textBox1.SelectionLength = blockLength;
+            //textBox1.SelectionBackColor = Color.Yellow;
+
         }
+
+
+        public void BuscarDiferencias()
+        {
+            diffs = DIFF.diff_main(textBox1.Text, textBox2.Text);
+            DIFF.diff_cleanupSemanticLossless(diffs);      // <--- see note !
+
+            chunklist1 = collectChunks(textBox1);
+            chunklist2 = collectChunks(textBox2);
+
+            paintChunks(textBox1, chunklist1);
+            paintChunks(textBox2, chunklist2);
+
+            textBox1.SelectionLength = 0;
+            textBox2.SelectionLength = 0;
+
+         }
+
+
+
+        List<Chunk> collectChunks(RichTextBox RTB)
+        {
+            RTB.Text = "";
+            List<Chunk> chunkList = new List<Chunk>();
+            foreach (Diff d in diffs)
+            {
+                if (RTB == textBox2 && d.operation == Operation.DELETE) continue;  // **
+                if (RTB == textBox1 && d.operation == Operation.INSERT) continue;  // **
+
+                Chunk ch = new Chunk();
+                int length = RTB.TextLength;
+                RTB.AppendText(d.text);
+                ch.startpos = length;
+                ch.length = d.text.Length;
+                ch.BackColor = RTB == textBox1 ? colors1[(int)d.operation] : colors2[(int)d.operation];
+                chunkList.Add(ch);
+            }
+            return chunkList;
+
+        }
+
+        void paintChunks(RichTextBox RTB, List<Chunk> theChunks)
+        {
+            foreach (Chunk ch in theChunks)
+            {
+                RTB.Select(ch.startpos, ch.length);
+                RTB.SelectionBackColor = ch.BackColor;
+            }
+
+        }
+
+
+
 
         public FrmComparacionDeElementos()
         {
@@ -33,7 +136,7 @@ namespace SQLMerge
 
         private void FrmComparacionDeElementos_Load(object sender, EventArgs e)
         {
-
+            BuscarDiferencias();
         }
 
       
@@ -50,13 +153,7 @@ namespace SQLMerge
             if (e.Control & e.KeyCode == Keys.A)
                 textBox2.SelectAll();
         }
-
-
-
-
-
-
-
+        
 
 
 
