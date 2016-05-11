9fd77f88731d6c4702d62a49074eb3985dd49493
acalcagno <calqui@gmail.com>
2016-05-11 12:43:19 -0300

    fileserver: Agregado log y reapuntadoa  calipso

--- FileServerRRHH/FileServerRRHH/Form1.Designer.cs
+++ FileServerRRHH/FileServerRRHH/Form1.Designer.cs
@@ -28,21 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtLog = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 12);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(579, 303);
+            this.txtLog.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 200);
+            this.ClientSize = new System.Drawing.Size(605, 328);
+            this.Controls.Add(this.txtLog);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "File Server";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        public System.Windows.Forms.TextBox txtLog;
     }
 }
 
--- FileServerRRHH/FileServerRRHH/Form1.cs
+++ FileServerRRHH/FileServerRRHH/Form1.cs
@@ -15,9 +15,7 @@ namespace FileServerRRHH
 {
     public partial class Form1 : Form
     {
-        private FileSystem file_system;
-        private ServicioDeCompresionYPersistenciaDeImagenes servicio_de_subida;
-        private string path = @"c:\imagenes";
+        private string path = @"\\calipso\digitalizacion_rrhh\";
 
         public Form1()
         {
@@ -26,48 +24,65 @@ namespace FileServerRRHH
         
         private void Form1_Load(object sender, EventArgs e)
         {
+            CheckForIllegalCrossThreadCalls = false;
             var timer = new System.Timers.Timer();
             timer.Elapsed += new ElapsedEventHandler(RevisarSiHayNovedades);
             timer.Interval = 1000;
             timer.Enabled = true;
+            txtLog.Text = "";
+            loguear("File Server inicializado");
         }
 
-        private static void RevisarSiHayNovedades(object source, ElapsedEventArgs e)
+        private void RevisarSiHayNovedades(object source, ElapsedEventArgs e)
         {
-            var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH;Integrated Security=True");
-            var file_system = new FileSystem();
-
-            TablaDeDatos novedades = conexion_db.Ejecutar("dbo.FS_GetNovedades");
-
-            foreach (var row in novedades.Rows)
+            try
             {
-                if (!(row.GetObject("request") is DBNull))
+                var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH;Integrated Security=True");
+                var file_system = new FileSystem();
+
+                TablaDeDatos novedades = conexion_db.Ejecutar("dbo.FS_GetNovedades");
+
+                foreach (var row in novedades.Rows)
                 {
-                    FileStream fs = new FileStream("c:\\imagenes\\" + row.GetInt("id"), FileMode.Open);
-                    Byte[] b = new Byte[fs.Length];
-                    fs.Read(b, 0, b.Length);
-                    fs.Close();                                  
+                    if (!(row.GetObject("nueva") is DBNull))
+                    {
+                        byte[] imageBytes = Encoding.ASCII.GetBytes(row.GetString("bytes_file"));
+                        FileStream fs = new FileStream(path + row.GetInt("id"), FileMode.Create);
+                        fs.Write(imageBytes, 0, imageBytes.Count());
+                        fs.Close();
 
-                    var parametros = new Dictionary<string, object>();
-                    parametros.Add("@id", row.GetInt("id"));
-                    parametros.Add("@bytes", Encoding.ASCII.GetString(b));
+                        var parametros = new Dictionary<string, object>();
+                        parametros.Add("@id", row.GetInt("id"));
 
-                    conexion_db.Ejecutar("dbo.FS_SubirArchivoPedido", parametros);
-                }
+                        conexion_db.Ejecutar("dbo.FS_ArchivoGuardadoConExito", parametros);
+                    }
 
-                if (!(row.GetObject("nueva") is DBNull))
-                {
-                    byte[] imageBytes = Encoding.ASCII.GetBytes(row.GetString("bytes_file"));
-                    FileStream fs = new FileStream("c:\\imagenes\\" + row.GetInt("id"), FileMode.Create);
-                    fs.Write(imageBytes, 0, imageBytes.Count());
-                    fs.Close();
+                    if (!(row.GetObject("request") is DBNull))
+                    {
+                        FileStream fs = new FileStream(path + row.GetInt("id"), FileMode.Open);
+                        Byte[] b = new Byte[fs.Length];
+                        fs.Read(b, 0, b.Length);
+                        fs.Close();
 
-                    var parametros = new Dictionary<string, object>();
-                    parametros.Add("@id", row.GetInt("id"));
+                        var parametros = new Dictionary<string, object>();
+                        parametros.Add("@id", row.GetInt("id"));
+                        parametros.Add("@bytes", Encoding.ASCII.GetString(b));
 
-                    conexion_db.Ejecutar("dbo.FS_ArchivoGuardadoConExito", parametros);
+                        conexion_db.Ejecutar("dbo.FS_SubirArchivoPedido", parametros);
+                    }
                 }
             }
+            catch (Exception error)
+            {
+                loguear("error:" + error.Message);
+            }
+        }
+
+        private void loguear(string mensaje)
+        {
+            txtLog.Text += "-----------------------------------------------" + "\r\n";
+            txtLog.Text += mensaje + "\r\n";
+            txtLog.Text += "-----------------------------------------------" + "\r\n";
         }
     }
 }
