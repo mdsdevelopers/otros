9a17a6a2a6c50f148cc0fc95239923d0cda00737
jlurgo <jlurgo@gmail.com>
2016-07-19 12:25:02 -0300

    subidor de imagenes de vehiculos

---
+++ SubidorImagenesVehiculos/SubidorImagenesVehiculos/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using General.Repositorios;
+using General.Modi;
+
+namespace SubidorImagenesVehiculos
+{
+    public partial class Form1 : Form
+    {
+        private string path = @"Y:\Vehiculos MDS";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH;Integrated Security=True");
+            var file_system = new FileSystem();
+
+            var imagenes = Directory.GetFiles(path, "*.jpg")
+                                     .Select(p => Path.GetFileName(p))
+                                     .ToList();
+            imagenes.ForEach(nombre_imagen =>
+            {
+                try
+                {
+                    var patente = nombre_imagen.Substring(0, 6);
+                    var parametros_bien = new Dictionary<string, object>();
+                    parametros_bien.Add("@patente", patente);
+                    var id_bien = conexion_db.EjecutarEscalar("dbo.MOBI_GetIdBienPorPatente", parametros_bien);
+
+                    Image img = file_system.getImagenFromPath(path + "\\" + nombre_imagen);
+                    var ms = new MemoryStream();
+                    img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    byte[] imageBytes = ms.ToArray();
+                    var bytesImagen = Convert.ToBase64String(imageBytes);
+
+                    var parametros_subir_imagen = new Dictionary<string, object>();
+                    parametros_subir_imagen.Add("@bytes", bytesImagen);
+                    var id_imagen = int.Parse(conexion_db.EjecutarEscalar("dbo.FS_SubirArchivo", parametros_subir_imagen).ToString());
+
+                    var parametros_asignar_imagen = new Dictionary<string, object>();
+                    parametros_asignar_imagen.Add("@idBien", id_bien);
+                    parametros_asignar_imagen.Add("@idImagen", id_imagen);
+                    conexion_db.Ejecutar("dbo.MOBI_AsignarImagenABien", parametros_asignar_imagen);
+
+                    file_system.moverArchivo(path + "\\" + nombre_imagen, path + "\\ImagenesSubidas");
+                }
+                catch (Exception)
+                {
+                    
+                }
+                
+            });
+        }
+    }
+}
