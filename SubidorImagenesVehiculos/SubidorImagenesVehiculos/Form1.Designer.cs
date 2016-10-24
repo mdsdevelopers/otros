b90b845907383e6fd9d0145d408750ec55d1d6be
jlurgo <jlurgo@gmail.com>
2016-10-24 20:19:59 -0300

    subidor vehiculos: cambio en interfaz

--- SubidorImagenesVehiculos/SubidorImagenesVehiculos/Form1.Designer.cs
+++ SubidorImagenesVehiculos/SubidorImagenesVehiculos/Form1.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.optAgregar = new System.Windows.Forms.RadioButton();
+            this.optReemplazar = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(248, 39);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,21 +44,59 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(11, 10);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(312, 20);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "C:\\ImagenesVehiculos";
+            // 
+            // optAgregar
+            // 
+            this.optAgregar.AutoSize = true;
+            this.optAgregar.Checked = true;
+            this.optAgregar.Location = new System.Drawing.Point(11, 42);
+            this.optAgregar.Name = "optAgregar";
+            this.optAgregar.Size = new System.Drawing.Size(62, 17);
+            this.optAgregar.TabIndex = 2;
+            this.optAgregar.TabStop = true;
+            this.optAgregar.Text = "Agregar";
+            this.optAgregar.UseVisualStyleBackColor = true;
+            // 
+            // optReemplazar
+            // 
+            this.optReemplazar.AutoSize = true;
+            this.optReemplazar.Location = new System.Drawing.Point(79, 42);
+            this.optReemplazar.Name = "optReemplazar";
+            this.optReemplazar.Size = new System.Drawing.Size(81, 17);
+            this.optReemplazar.TabIndex = 3;
+            this.optReemplazar.Text = "Reemplazar";
+            this.optReemplazar.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(116, 50);
+            this.ClientSize = new System.Drawing.Size(334, 71);
+            this.Controls.Add(this.optReemplazar);
+            this.Controls.Add(this.optAgregar);
+            this.Controls.Add(this.txtPath);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Subidor Imágenes Vehículos";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.RadioButton optAgregar;
+        private System.Windows.Forms.RadioButton optReemplazar;
     }
 }
 
--- SubidorImagenesVehiculos/SubidorImagenesVehiculos/Form1.cs
+++ SubidorImagenesVehiculos/SubidorImagenesVehiculos/Form1.cs
@@ -14,7 +14,6 @@ namespace SubidorImagenesVehiculos
 {
     public partial class Form1 : Form
     {
-        private string path = @"Y:\Vehiculos MDS";
         public Form1()
         {
             InitializeComponent();
@@ -22,44 +21,58 @@ namespace SubidorImagenesVehiculos
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var path = txtPath.Text;
+            var reemplazar_imagenes = optReemplazar.Checked;
             var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH;Integrated Security=True");
             var file_system = new FileSystem();
 
-            var imagenes = Directory.GetFiles(path, "*.jpg")
-                                     .Select(p => Path.GetFileName(p))
-                                     .ToList();
-            imagenes.ForEach(nombre_imagen =>
+            var carpetas = Directory.GetDirectories(path)
+                                    .ToList();
+
+            carpetas.ForEach(carpeta =>
             {
-                try
+                var patente = new DirectoryInfo(carpeta).Name.Replace(" ", "");
+                var imagenes = Directory.GetFiles(carpeta, "*.jpg")
+                                         .Select(p => Path.GetFileName(p))
+                                         .ToList();
+
+                imagenes.ForEach(nombre_imagen =>
                 {
-                    var patente = nombre_imagen.Substring(0, 6);
-                    var parametros_bien = new Dictionary<string, object>();
-                    parametros_bien.Add("@patente", patente);
-                    var id_bien = conexion_db.EjecutarEscalar("dbo.MOBI_GetIdBienPorPatente", parametros_bien);
+                    try
+                    {
+                        var parametros_bien = new Dictionary<string, object>();
+                        parametros_bien.Add("@patente", patente);
+                        var id_bien = conexion_db.EjecutarEscalar("dbo.MOBI_GetIdBienPorPatente", parametros_bien);
 
-                    Image img = file_system.getImagenFromPath(path + "\\" + nombre_imagen);
-                    var ms = new MemoryStream();
-                    img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-                    byte[] imageBytes = ms.ToArray();
-                    var bytesImagen = Convert.ToBase64String(imageBytes);
+                        Image img = file_system.getImagenFromPath(carpeta + "\\" + nombre_imagen);
+                        var ms = new MemoryStream();
+                        img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        byte[] imageBytes = ms.ToArray();
+                        var bytesImagen = Convert.ToBase64String(imageBytes);
 
-                    var parametros_subir_imagen = new Dictionary<string, object>();
-                    parametros_subir_imagen.Add("@bytes", bytesImagen);
-                    var id_imagen = int.Parse(conexion_db.EjecutarEscalar("dbo.FS_SubirArchivo", parametros_subir_imagen).ToString());
+                        var parametros_subir_imagen = new Dictionary<string, object>();
+                        parametros_subir_imagen.Add("@bytes", bytesImagen);
+                        var id_imagen = int.Parse(conexion_db.EjecutarEscalar("dbo.FS_SubirArchivo", parametros_subir_imagen).ToString());
 
-                    var parametros_asignar_imagen = new Dictionary<string, object>();
-                    parametros_asignar_imagen.Add("@idBien", id_bien);
-                    parametros_asignar_imagen.Add("@idImagen", id_imagen);
-                    conexion_db.Ejecutar("dbo.MOBI_AsignarImagenABien", parametros_asignar_imagen);
+                        var parametros_asignar_imagen = new Dictionary<string, object>();
+                        parametros_asignar_imagen.Add("@idBien", id_bien);
+                        parametros_asignar_imagen.Add("@idImagen", id_imagen);
+                        conexion_db.Ejecutar("dbo.MOBI_AsignarImagenABien", parametros_asignar_imagen);
 
-                    file_system.moverArchivo(path + "\\" + nombre_imagen, path + "\\ImagenesSubidas");
-                }
-                catch (Exception)
-                {
-                    
-                }
-                
+                        file_system.moverArchivo(carpeta + "\\" + nombre_imagen, carpeta + "\\ImagenesSubidas");
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+
+                });
             });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
