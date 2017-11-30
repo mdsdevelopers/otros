54d401c921bc2c6e7150da9dae535a8d9d7389f0
aazaa <arielzambrano@gmail.com>
2017-11-29 23:34:38 -0300

    Modificacion para bajada de imagenes de credencial y excel de credenciales vigentes

--- SincronizadorControlDeAsistencia/SincronizadorControlDeAsistencia/Form1.Designer.cs
+++ SincronizadorControlDeAsistencia/SincronizadorControlDeAsistencia/Form1.Designer.cs
@@ -30,6 +30,19 @@
         {
             this.btnDescargar = new System.Windows.Forms.Button();
             this.dtpFechaDesde = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_destino = new System.Windows.Forms.TextBox();
+            this.btn_cambiarRutaImagenes = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_destinoCredenciales = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnDescargar
@@ -37,9 +50,10 @@
             this.btnDescargar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDescargar.Location = new System.Drawing.Point(12, 38);
+            this.btnDescargar.ForeColor = System.Drawing.Color.Black;
+            this.btnDescargar.Location = new System.Drawing.Point(6, 91);
             this.btnDescargar.Name = "btnDescargar";
-            this.btnDescargar.Size = new System.Drawing.Size(553, 38);
+            this.btnDescargar.Size = new System.Drawing.Size(401, 39);
             this.btnDescargar.TabIndex = 0;
             this.btnDescargar.Text = "Descargar";
             this.btnDescargar.UseVisualStyleBackColor = true;
@@ -49,20 +63,141 @@
             // 
             this.dtpFechaDesde.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dtpFechaDesde.Location = new System.Drawing.Point(12, 12);
+            this.dtpFechaDesde.Location = new System.Drawing.Point(94, 31);
             this.dtpFechaDesde.Name = "dtpFechaDesde";
-            this.dtpFechaDesde.Size = new System.Drawing.Size(553, 20);
+            this.dtpFechaDesde.Size = new System.Drawing.Size(313, 22);
             this.dtpFechaDesde.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txt_destinoCredenciales);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(12, 181);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(413, 111);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Credenciales Activas";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_cambiarRutaImagenes);
+            this.groupBox2.Controls.Add(this.txt_destino);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnDescargar);
+            this.groupBox2.Controls.Add(this.dtpFechaDesde);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(413, 143);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Imágenes";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(7, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar Archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fecha:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Guardar en:";
+            // 
+            // txt_destino
+            // 
+            this.txt_destino.Enabled = false;
+            this.txt_destino.Location = new System.Drawing.Point(94, 64);
+            this.txt_destino.Name = "txt_destino";
+            this.txt_destino.Size = new System.Drawing.Size(282, 22);
+            this.txt_destino.TabIndex = 5;
+            this.txt_destino.Text = "C:\\ImagenesSincronizadas\\";
+            // 
+            // btn_cambiarRutaImagenes
+            // 
+            this.btn_cambiarRutaImagenes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_cambiarRutaImagenes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cambiarRutaImagenes.ForeColor = System.Drawing.Color.Black;
+            this.btn_cambiarRutaImagenes.Location = new System.Drawing.Point(379, 63);
+            this.btn_cambiarRutaImagenes.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_cambiarRutaImagenes.Name = "btn_cambiarRutaImagenes";
+            this.btn_cambiarRutaImagenes.Size = new System.Drawing.Size(28, 22);
+            this.btn_cambiarRutaImagenes.TabIndex = 6;
+            this.btn_cambiarRutaImagenes.Text = "....";
+            this.btn_cambiarRutaImagenes.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_cambiarRutaImagenes.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btn_cambiarRutaImagenes.UseVisualStyleBackColor = true;
+            this.btn_cambiarRutaImagenes.Click += new System.EventHandler(this.btn_cambiarRutaImagenes_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Guardar en:";
+            // 
+            // txt_destinoCredenciales
+            // 
+            this.txt_destinoCredenciales.Enabled = false;
+            this.txt_destinoCredenciales.Location = new System.Drawing.Point(102, 28);
+            this.txt_destinoCredenciales.Name = "txt_destinoCredenciales";
+            this.txt_destinoCredenciales.Size = new System.Drawing.Size(275, 22);
+            this.txt_destinoCredenciales.TabIndex = 8;
+            this.txt_destinoCredenciales.Text = "C:\\CredencialesVigentes\\";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(379, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = ".....";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 88);
-            this.Controls.Add(this.dtpFechaDesde);
-            this.Controls.Add(this.btnDescargar);
+            this.ClientSize = new System.Drawing.Size(432, 322);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Sincronizador";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -71,6 +206,17 @@
 
         private System.Windows.Forms.Button btnDescargar;
         private System.Windows.Forms.DateTimePicker dtpFechaDesde;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_cambiarRutaImagenes;
+        private System.Windows.Forms.TextBox txt_destino;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_destinoCredenciales;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- SincronizadorControlDeAsistencia/SincronizadorControlDeAsistencia/Form1.cs
+++ SincronizadorControlDeAsistencia/SincronizadorControlDeAsistencia/Form1.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using General.Repositorios;
 using General.Modi;
 using System.IO;
+using System.Configuration;
 
 namespace SincronizadorControlDeAsistencia
 {
@@ -21,13 +22,27 @@ namespace SincronizadorControlDeAsistencia
 
         private void btnDescargar_Click(object sender, EventArgs e)
         {
-            var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH;Integrated Security=True");
-            var file_system = new FileSystem();
+            try
+            {
 
+            int cantidad_imagenes = 0;
+            var connectionString = ConfigurationManager.ConnectionStrings["Sql"].ConnectionString;
+
+            // var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH_DESA;Integrated Security=True");
+            var conexion_db = new ConexionBDSQL(connectionString);
+            
+                
+            var file_system = new FileSystem();
             var parametros = new Dictionary<string, object>();
+
+            Cursor.Current = Cursors.WaitCursor;
+
             parametros.Add("@fecha_desde", dtpFechaDesde.Value);
             var tablaIds = conexion_db.Ejecutar("dbo.Acre_GetImagenesUltimasCredenciales", parametros);
 
+            if (!tablaIds.Rows.Any()) { Cursor.Current = Cursors.Default; MessageBox.Show("No se encontraron imágenes con la fecha indicada", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Information); return; }
+
+           
 
             tablaIds.Rows.ForEach((row) =>
             {
@@ -61,10 +76,94 @@ namespace SincronizadorControlDeAsistencia
 
                         ms.Write(imageBytes, 0, imageBytes.Length);
                         Image imagen = Image.FromStream(ms, true);
-                        imagen.Save("c:\\imagenes\\bla\\" + row.GetInt("nroDocumento") + ".jpg");
+                       // imagen.Save("c:\\imagenes\\bla\\" + row.GetInt("nroDocumento") + ".jpg");
+
+                        if (!Directory.Exists(txt_destino.Text))
+                        {
+                            Directory.CreateDirectory(txt_destino.Text); 
+                        }
+                       
+                        imagen.Save(txt_destino.Text + row.GetInt("nroDocumento") + ".jpg");
                     }
 
                 });
+            }
+            Cursor.Current = Cursors.Default;
+            cantidad_imagenes = imagenes_descargadas.Count;
+            MessageBox.Show("Proceso finalizado. Se descargaron " + cantidad_imagenes.ToString() + " imágenes", "Resultado de Operación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex )
+            {
+                Cursor.Current = Cursors.Default;
+                MessageBox.Show("Ocurrió una excepción:" + ex.Message + "-" + ex.InnerException, "Validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+        }
+        
+        private void btn_cambiarRutaImagenes_Click(object sender, EventArgs e)
+        {
+            DialogResult button1_Click = folderBrowserDialog1.ShowDialog();
+            txt_destino.Text = folderBrowserDialog1.SelectedPath;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult button2_Click = folderBrowserDialog1.ShowDialog();
+            txt_destinoCredenciales.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                     
+
+            var connectionString = ConfigurationManager.ConnectionStrings["Sql"].ConnectionString;
+                
+            // var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH_DESA;Integrated Security=True");
+            var conexion_db = new ConexionBDSQL(connectionString);
+            
+            var file_system = new FileSystem();
+
+            Cursor.Current = Cursors.WaitCursor;
+            var tablaIds = conexion_db.Ejecutar("dbo.Acre_MigracionCredencialesActivas");
+            
+            if (!tablaIds.Rows.Any()) { MessageBox.Show("No se encontraron credenciales", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Information); return; }
+
+            //
+            var lines = new List<string>();
+
+            string[] columnNames = tablaIds.Columns.Cast<DataColumn>().
+                                              Select(column => column.ColumnName).
+                                              ToArray();
+
+            var header = string.Join(",", columnNames);
+            lines.Add(header);
+
+            var valueLines = tablaIds.AsEnumerable()
+                               .Select(row => string.Join(",", row.ItemArray));
+            lines.AddRange(valueLines);
+
+            if (!Directory.Exists(txt_destinoCredenciales.Text))
+            {
+                Directory.CreateDirectory(txt_destinoCredenciales.Text);
+            }
+           
+            File.WriteAllLines(txt_destinoCredenciales.Text+"\\"+ "Credenciales.csv", lines);
+            Cursor.Current = Cursors.Default;
+            MessageBox.Show("Proceso finalizado. Se encontraron " + tablaIds.Rows.Count.ToString() + " registros", "Resultado de Operación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //
+                
+            }
+            catch (Exception ex)
+            {
+                Cursor.Current = Cursors.Default;
+                MessageBox.Show("Ocurrió una excepción:" + ex.Message + "-" + ex.InnerException, "Validación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
         }
     }
