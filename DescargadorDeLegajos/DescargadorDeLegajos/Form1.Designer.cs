8b1cb4f79bbced80bf87f06e69bd0c044a7605c8
jlurgo <jlurgo@gmail.com>
2019-03-12 21:08:46 -0300

    Descargador

--- DescargadorDeLegajos/DescargadorDeLegajos/Form1.Designer.cs
+++ DescargadorDeLegajos/DescargadorDeLegajos/Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.txtLegajo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.AllowDrop = true;
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(262, 38);
+            this.button1.Location = new System.Drawing.Point(500, 193);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -48,16 +49,27 @@
             // 
             this.txtLegajo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtLegajo.Location = new System.Drawing.Point(12, 12);
+            this.txtLegajo.Location = new System.Drawing.Point(15, 25);
+            this.txtLegajo.Multiline = true;
             this.txtLegajo.Name = "txtLegajo";
-            this.txtLegajo.Size = new System.Drawing.Size(325, 20);
+            this.txtLegajo.Size = new System.Drawing.Size(563, 66);
             this.txtLegajo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Legajos separados por coma";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(350, 85);
+            this.ClientSize = new System.Drawing.Size(587, 228);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtLegajo);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -71,6 +83,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtLegajo;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- DescargadorDeLegajos/DescargadorDeLegajos/Form1.cs
+++ DescargadorDeLegajos/DescargadorDeLegajos/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 using General.Repositorios;
 using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace DescargadorDeLegajos
 {
@@ -21,18 +23,38 @@ namespace DescargadorDeLegajos
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH;Integrated Security=True");
-            var parametros = new Dictionary<string, object>();
+            var ids = txtLegajo.Text.Split(',');
 
-            parametros.Add("@id_interna", int.Parse(txtLegajo.Text));
-            var tablaImagenes = conexion_db.Ejecutar("dbo.MODI_Imagenes_De_Un_Legajo", parametros);
+            foreach (string textid in ids)
+            {
+                descargarLegajo(int.Parse(textid));
+            }            
+        }
+
+        private void descargarLegajo(int legajo)
+        {
+            var conexion_db = new ConexionBDSQL("Data Source=10.80.5.5;Initial Catalog=DB_RRHH;Integrated Security=True");
+
+            var parametros_imagenes = new Dictionary<string, object>();
+            parametros_imagenes.Add("@id_interna", legajo);
+            var tablaImagenes = conexion_db.Ejecutar("dbo.MODI_Imagenes_De_Un_Legajo", parametros_imagenes);
+
+            var parametros_docs = new Dictionary<string, object>();
+            parametros_docs.Add("@legajo", legajo);
+            parametros_docs.Add("@id", legajo);
+            var tablaDocs = conexion_db.Ejecutar("dbo.LEG_GET_Indice_Documentos", parametros_docs);
+
+            string bytes_file;
+            byte[] imageBytes;
+            iTextSharp.text.Image pic;
+
             tablaImagenes.Rows.ForEach((row) =>
             {
                 var parametros_foto = new Dictionary<string, object>();
 
                 parametros_foto.Add("@id_imagen", row.GetInt("id_imagen"));
                 var tablaFoto = conexion_db.Ejecutar("dbo.MODI_Get_Imagen", parametros_foto);
-
+                
                 var id_foto = tablaFoto.Rows[0].GetInt("id_archivo");
 
                 FileStream fs = new FileStream(path + id_foto, FileMode.Open);
@@ -40,17 +62,61 @@ namespace DescargadorDeLegajos
                 fs.Read(b, 0, b.Length);
                 fs.Close();
 
-                string bytes_file = Encoding.ASCII.GetString(b);
+                bytes_file = Encoding.ASCII.GetString(b);
 
-                byte[] imageBytes = Convert.FromBase64String(bytes_file);
+                imageBytes = Convert.FromBase64String(bytes_file);
 
                 MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
 
                 ms.Write(imageBytes, 0, imageBytes.Length);
-                Image imagen = Image.FromStream(ms, true);
+                System.Drawing.Image imagen = System.Drawing.Image.FromStream(ms, true);
 
-                imagen.Save("C:\\imagenes\\" + id_foto + ".jpg");
+                if (row.GetObject("id_documento") == null) return;
+                var nombre_doc = GetNombreDoc(row.GetInt("id_documento"), tablaDocs);
+
+                Document document = new Document(PageSize.LETTER);
+                PdfWriter.GetInstance(document, new FileStream("C:\\imagenes\\" + legajo + '_' + nombre_doc + ".pdf", FileMode.Create));
+                document.Open();
+
+                pic = iTextSharp.text.Image.GetInstance(imagen, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                if (pic.Height > pic.Width)
+                {
+                    //Maximum height is 800 pixels.
+                    float percentage = 0.0f;
+                    percentage = 700 / pic.Height;
+                    pic.ScalePercent(percentage * 100);
+                }
+                else
+                {
+                    //Maximum width is 600 pixels.
+                    float percentage = 0.0f;
+                    percentage = 540 / pic.Width;
+                    pic.ScalePercent(percentage * 100);
+                }
+
+                document.Add(pic);
+                document.NewPage();
+                document.Close();
+                ms.Dispose();                
+                //imagen.Save("C:\\imagenes\\" + txtLegajo.Text + '_' + id_foto + ".jpg");
             });
+
+
+        }
+
+        private string GetNombreDoc(int id_doc, TablaDeDatos tablaDocs)
+        {
+            String nombre = "";
+            tablaDocs.Rows.ForEach((row) =>
+            {
+                var id_doc_en_tabla = row.GetInt("id");
+                if (id_doc == id_doc_en_tabla)
+                {
+                    nombre = row.GetString("TIPO_LUE");
+                }
+            });
+            return nombre.Replace("\\", "-").Replace("/", "-").Replace(":", "-");
         }
     }
 }
